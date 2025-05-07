faf2dd81fddd16d388448f681e7757c9dbc179a2
ardianpramana <paramana.ardian@gmail.com>
2025-05-07 11:54:14 +0700

    [ADD] agnes_academy: add compute sample

--- agnes_academy/models/course.py
+++ agnes_academy/models/course.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api, _
+from datetime import date
 
 
 DOMAIN_USER_ID = [('name', 'ilike', 'XXX')]
@@ -7,11 +8,16 @@ class Course(models.Model):
     _name = 'course.course'
     _description = 'Course'
 
+    def default_start_date_first(self):
+        return date.today()
+
     # day 1 - phase 1
     name = fields.Char(string='Title')
     description = fields.Text(string='Description')
-    duration = fields.Integer(string='Duration (hours)')
-    start_date = fields.Date(string='Start Date')
+    duration = fields.Integer(
+        string='Duration (hours)',
+        default=10,
+    )
 
     # Day 1 - phase 2
     syllabus = fields.Html(
@@ -26,7 +32,15 @@ class Course(models.Model):
         ('advance', 'Advance'),
         ('expert', 'Expert'),
     ], string='Level', default='beginner')
-    start_date = fields.Date(string='Start Date')
+
+    start_date = fields.Date(
+        string='Start Date',
+        default=fields.Date.today,
+        # default=lambda self: date.today(),
+        # default=default_start_date_first,
+        # default=lambda self: self.default_start_date_second(),
+        tracking=True
+    )
     end_date = fields.Date(string='End Date')
     enrollment_deadline = fields.Datetime(string='Enrollment Deadline')
 
@@ -45,12 +59,16 @@ class Course(models.Model):
         string='Responsible',
         domain=DOMAIN_USER_ID,
         default=lambda self: self.env.user, index=True)
+    user_email = fields.Char(
+        string="Responsible Email",
+        compute='_compute_user_email',
+        store=False
+    )
     session_ids = fields.One2many(
         comodel_name='course.session',
         inverse_name='course_id',
         string='Sessions'
     )
-
     company_id = fields.Many2one(
         'res.company',
         string='Company',
@@ -59,6 +77,24 @@ class Course(models.Model):
         default=lambda self: self.env.company)
     active = fields.Boolean(default=True)
     is_to_delete = fields.Boolean()
+
+    # @api.onchange('user_id')
+    # @api.onchange()
+    # @api.depends('user_id')
+    def _compute_user_email(self):
+        for record in self:
+            if record.user_id:
+                record.user_email = record.user_id.partner_id.email
+            else:
+                record.user_email = False
+
+    @api.onchange('start_date')
+    def _onchange_start_date(self):
+        if self.start_date:
+            self.end_date = self.start_date
+
+    def default_start_date_second(self):
+        return date.today()
 
     def button_publish(self):
         self.state = 'enrollment'
